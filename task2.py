9fb1cf0b3786a01a943cb0f2ce1b32b96714d8bb
Darina Khalina <darina.khalina@namecheap.com>
2024-01-21 19:16:58 +0200

    chore(hw6): add task 2

--- task2.py
+++ task2.py
@@ -1,38 +1,7 @@
 from collections import deque
 import networkx as nx
-import matplotlib.pyplot as plt
-
-
-def dfs_recursive(graph, vertex, visited=None):
-    if visited is None:
-        visited = set()
-    visited.add(vertex)
-    print(vertex, end=" ")  # Відвідуємо вершину
-    for neighbor in graph[vertex]:
-        if neighbor not in visited:
-            dfs_recursive(graph, neighbor, visited)
-
-
-def bfs_recursive(graph, queue, visited=None):
-    # Перевіряємо, чи існує множина відвіданих вершин, якщо ні, то ініціалізуємо нову
-    if visited is None:
-        visited = set()
-    # Якщо черга порожня, завершуємо рекурсію
-    if not queue:
-        return
-    # Вилучаємо вершину з початку черги
-    vertex = queue.popleft()
-    # Перевіряємо, чи відвідували раніше дану вершину
-    if vertex not in visited:
-        # Якщо не відвідували, друкуємо вершину
-        print(vertex, end=" ")
-        # Додаємо вершину до множини відвіданих вершин.
-        visited.add(vertex)
-        # Додаємо невідвіданих сусідів даної вершини в кінець черги.
-        queue.extend(set(graph[vertex]) - visited)
-    # Рекурсивний виклик функції з тією ж чергою та множиною відвіданих вершин
-    bfs_recursive(graph, queue, visited)
-
+from dfs import dfs_recursive
+from bfs import bfs_recursive
 
 # Граф
 graph = {
@@ -54,9 +23,7 @@ graph = {
 G = nx.Graph(graph)
 
 dfs_tree = nx.dfs_tree(G, "0")
-print(list(dfs_tree.nodes()))
 dfs_recursive(graph, "0")
 
 bfs_tree = nx.bfs_tree(G, "0")
-print(list(bfs_tree.nodes()))
 bfs_recursive(graph, deque(["0"]))
